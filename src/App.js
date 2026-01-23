5caa624ea8bef07a8402494217c473f24e32e9b2
sia20015-stack <sia20015@gmail.com>
2026-01-23 10:54:38 +0900

    레이아웃 작업

--- src/App.js
+++ src/App.js
@@ -12,23 +12,30 @@ import Channel from './pages/Channel'
 import Video from './pages/Video'
 import Search from './pages/Search'
 import Not from './pages/Not'
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/today' element={<Today/>}/>
-        <Route path='/music' element={<Music/>}/>
-        <Route path='/movie' element={<Movie/>}/>
-        <Route path='/book' element={<Book/>}/>
-        <Route path='/anime' element={<Anime/>}/>
-        <Route path='/youtube' element={<Youtube/>}/>
-        <Route path='/channel/:chennelID' element={<Channel/>}/>
-        <Route path='/video/:videoID' element={<Video/>}/>
-        <Route path='/search/:searchID' element={<Search/>}/>
-        <Route path='/*'element={<Not/>}/>
-      </Routes>
+      <Header/>
+      <Main>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/today' element={<Today/>}/>
+          <Route path='/music' element={<Music/>}/>
+          <Route path='/movie' element={<Movie/>}/>
+          <Route path='/book' element={<Book/>}/>
+          <Route path='/anime' element={<Anime/>}/>
+          <Route path='/youtube' element={<Youtube/>}/>
+          <Route path='/channel/:chennelID' element={<Channel/>}/>
+          <Route path='/video/:videoID' element={<Video/>}/>
+          <Route path='/search/:searchID' element={<Search/>}/>
+          <Route path='/*'element={<Not/>}/>
+        </Routes>
+      </Main>
+      <Footer/>
     </BrowserRouter>
   )
 }
